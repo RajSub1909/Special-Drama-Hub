1cde3b63bbdbcac7762d21262144a29629ba25c3
RajSub1909 <rajshukla1602@gmail.com>
2026-08-14 09:26:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://njutjrrlzvtcaiqarlfp.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_CvfbSLnl7lqfu2W3lArZTg_vWdCR-sh";
 
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
